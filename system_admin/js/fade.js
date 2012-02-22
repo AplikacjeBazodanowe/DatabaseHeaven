bb536bd7eeb1b99184ded7919fa2560ebfda9f74
Jacek Topolski <jajcek.30@gmail.com>
2012-02-22 22:02:37 +0100

    poprawki cd xD

--- system_admin/js/fade.js
+++ system_admin/js/fade.js
@@ -1,1 +1,1 @@
-function add_toggle() {	var dark = document.getElementById( "dark" );	var add = document.getElementById( "adding" );	dark.style.visibility = dark.style.visibility != 'visible' ? 'visible' : 'hidden';	adding.style.visibility = adding.style.visibility != 'visible' ? 'visible' : 'hidden';}
+function add_toggle() {	var dark = document.getElementById( "dark" );	var add = document.getElementById( "adding" );	dark.style.visibility = dark.style.visibility != 'visible' ? 'visible' : 'hidden';	add.style.visibility = add.style.visibility != 'visible' ? 'visible' : 'hidden';}
