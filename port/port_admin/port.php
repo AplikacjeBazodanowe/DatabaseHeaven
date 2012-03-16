6bfb316da1e1603c241763a5fea159bb1e8632e7
Jacek Topolski <jajcek.30@gmail.com>
2012-03-16 14:52:16 +0100

    dokonczony panel admina portu

--- port/port_admin/port.php
+++ port/port_admin/port.php
@@ -13,12 +13,27 @@
 			<div class="menu grad grayBorders">
 				<a href="?menu=ships"><input class="button baseFont menu_button" type="button" value="Docked ships"></a>
 				<a href="?menu=history"><input class="button baseFont menu_button" type="button" value="History"></a>
+				<a href="?menu=payments"><input class="button baseFont menu_button" type="button" value="Payments"></a>
 			</div>
+			<div id="adding" class="add_edit">
+				<?php 
+					if( isset( $_GET['menu'] ) && $_GET['menu'] == "payments" )
+						include( "payments/payments_adding.php" );
+				?>
+			</div>
+			<div id="editing" class="add_edit">
+				<?php 
+					if( isset( $_GET['menu'] ) && $_GET['menu'] == "payments" )
+						include( "payments/payments_editing.php" );
+				?>
+			</div>			
 			<div class="left_group grad grayBorders">
 				<?php 
 					if( isset( $_GET['menu'] ) ) {
 						if( $_GET['menu'] == "history" ) {
 							include( "history/history_left_group.php" );
+						} elseif( $_GET['menu'] == "payments" ) {
+							include( "payments/payments_left_group.php" );
 						} else {
 							include( "ships/ships_left_group.php" );
 						}
@@ -32,6 +47,8 @@
 					if( isset( $_GET['menu'] ) ) {
 						if( $_GET['menu'] == "history" ) {
 							include( "history/history_desc_field.php" );
+						} elseif( $_GET['menu'] == "payments" ) {
+							include( "payments/payments_desc_field.php" );
 						} else {
 							include( "ships/ships_desc_field.php" );
 						}
